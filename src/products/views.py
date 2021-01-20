3dfaeb078809e44f3d24c9bf182e07d4793a44d6
surajmt8848 <suraj8848mt@gmail.com>
2021-01-20 18:33:02 +0545

    create views and models for products

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        
+
+    def destroy(self, request, pk=None):
+        pass
